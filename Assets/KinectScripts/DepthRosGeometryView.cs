a4f953402ccd705aecd8cec0b58b3c65d26f93e7
Amey Barapatre <amey.barapatre@gmail.com>
2017-11-12 20:24:33 -0600

    Intial Build

---
+++ Assets/KinectScripts/DepthRosGeometryView.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Windows.Kinect;
+using System.Runtime.InteropServices;
+using System;
+
+public class DepthRosGeometryView : MonoBehaviour {
+    private CoordinateMapper m_pCoordinateMapper;
+    private MultiSourceFrameReader m_pMultiSourceFrameReader;
+    private DepthSpacePoint[] m_pDepthCoordinates;
+
+    //private WebsocketClient wsc;
+    string depthTopic;
+    string colorTopic;
+    int framerate = 100;
+    public string compression = "none"; //"png" is the other option, haven't tried it yet though
+    string depthMessage;
+    string colorMessage;
+    private KinectManager insta;
+
+    public Material Material;
+    public static Texture2D depthTexture;
+    public static Texture2D colorTexture;
+  
+    int width = 512;
+    int height = 424;
+
+    Matrix4x4 m;
+
+    // Use this for initialization
+    void Start() {
+        // Create a texture for the depth image and color image
+        depthTexture = new Texture2D(width, height, TextureFormat.R16, false);
+        colorTexture = new Texture2D(2, 2);
+
+        /*
+        wsc = GameObject.Find("WebsocketClient").GetComponent<WebsocketClient>();
+        depthTopic = "kinect2/sd/image_depth_rect";
+        colorTopic = "kinect2/sd/image_color_rect/compressed";
+        wsc.Subscribe(depthTopic, "sensor_msgs/Image", compression, framerate);
+        wsc.Subscribe(colorTopic, "sensor_msgs/CompressedImage", compression, framerate);*/
+        InvokeRepeating("UpdateTexture", 0.1f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void UpdateTexture() {
+        try {
+            insta = KinectManager.Instance;
+            ushort[] source = insta.GetRawDepthMap();
+            byte[]  depthImage  = new byte[source.Length * 2]; 
+            Buffer.BlockCopy(source, 0, depthImage, 0, source.Length * 2);
+
+            depthTexture = insta.GetUsersLblTex();
+            //depthTexture.LoadImage(depthImage);
+            depthTexture.Apply();
+            Debug.Log(depthImage.Length);
+
+        }
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+
+        try {
+            insta = KinectManager.Instance;
+            colorMessage = "hello";
+            byte[] colorImage = Color32ArrayToByteArray(insta.getColor()) ;
+            colorTexture = insta.GetUsersClrTex();
+            colorTexture.Apply();
+            Debug.Log(colorImage.Length);
+        }
+        catch {
+            return;
+        }
+    }
+
+    void OnRenderObject() {
+        insta = KinectManager.Instance;
+        Material.SetTexture("_MainTex", insta.GetUsersLblTex());
+        //Material.SetTexture("_ColorTex", colorTexture);
+        Material.SetPass(0);
+
+           // m = Matrix4x4.TRS(this.transform.position, this.transform.rotation, this.transform.localScale);
+        //Material.SetMatrix("transformationMatrix", m);
+
+        Graphics.DrawProcedural(MeshTopology.Points, 512 * 424, 1);
+      
+    }
+
+    private static byte[] Color32ArrayToByteArray(Color32[] colors)
+    {
+    int length = 4 * colors.Length;
+    byte[] bytes = new byte[length];
+    IntPtr ptr = Marshal.AllocHGlobal(length);
+    Marshal.StructureToPtr(colors, ptr, true);
+    Marshal.Copy(ptr, bytes, 0, length);
+    Marshal.FreeHGlobal(ptr);
+    return bytes;
+    }
+
+    void onGUI(){
+       
+    }
+    public static Texture2D sendcolor()
+    { return DepthRosGeometryView.colorTexture; }
+}
+ 
---
+++ Assets/KinectScripts/DepthView.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Windows.Kinect;
+using System.Runtime.InteropServices;
+using System;
+
+
+public class DepthView : MonoBehaviour
+{
+ 
+    private Mesh _Mesh;
+    private Vector3[] _Vertices;
+    private Vector2[] _UV;
+    private int[] _Triangles;
+    private int[] _Indices;
+    private Color[] _Colors;
+
+    // Only works at 4 right now
+    private const int _DownsampleSize = 4 ;
+    private const double _DepthScale = 0.01f;
+    private const int _Speed = 50;
+    private KinectManager insta;
+    public Boolean ShowTrueColors = true;
+    void Start()
+    {
+        // Downsample to lower resolution
+            CreateMesh(KinectWrapper.GetDepthWidth() / _DownsampleSize, KinectWrapper.GetDepthHeight() / _DownsampleSize);
+
+       
+    }
+
+    void CreateMesh(int width, int height)
+    {
+        _Mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _Mesh;
+        _Colors = new Color[width * height];
+        _Vertices = new Vector3[width * height];
+        _UV = new Vector2[width * height];
+        _Triangles = new int[6 * ((width - 1) * (height - 1))];
+        _Indices = new int[width * height];
+        int triangleIndex = 0;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = (y * width) + x;
+                _Indices[index] = index;
+                _Vertices[index] = new Vector3(x, -y, 0);
+                _UV[index] = new Vector2(((float)x / (float)width), ((float)y / (float)height));
+                _Colors[index] = new Color(255f, 255f, 255f, 1.0f);
+                // Skip the last row/col
+                if (x != (width - 1) && y != (height - 1))
+                {
+                    int topLeft = index;
+                    int topRight = topLeft + 1;
+                    int bottomLeft = topLeft + width;
+                    int bottomRight = bottomLeft + 1;
+
+                    _Triangles[triangleIndex++] = topLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomRight;
+                }
+            }
+        }
+
+        _Mesh.vertices = _Vertices;
+        _Mesh.uv = _UV;
+        //_Mesh.triangles = _Triangles;
+       // _Mesh.RecalculateNormals();
+
+        _Mesh.SetIndices(_Indices, MeshTopology.Points, 0);
+        _Mesh.colors = _Colors;
+    }
+
+    void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+        GUI.TextField(new Rect(Screen.width - 250, 10, 250, 20), "DepthMode");
+        GUI.EndGroup();
+    }
+
+    void Update()
+    {
+            insta = KinectManager.Instance;
+            //gameObject.GetComponent<Renderer>().material.mainTexture = insta.GetUsersClrTex();
+            RefreshData(insta.GetRawDepthMap(),insta.colorsize()[0], insta.colorsize()[1]);
+       
+         
+    }
+
+    private void RefreshData(ushort[] depthData, int colorWidth, int colorHeight)
+    {
+        insta = KinectManager.Instance;
+
+        
+        for (int y = 0; y < KinectWrapper.Constants.DepthImageHeight; y += _DownsampleSize)
+        {
+            for (int x = 0; x < KinectWrapper.Constants.DepthImageWidth; x += _DownsampleSize)
+            {
+                int indexX = x / _DownsampleSize;
+                int indexY = y / _DownsampleSize;
+                int smallIndex = (indexY * (KinectWrapper.GetDepthWidth() / _DownsampleSize)) + indexX;
+
+                double avg = GetAvg(depthData, x, y, KinectWrapper.GetDepthWidth(), KinectWrapper.GetDepthHeight());
+
+                avg = avg * _DepthScale;
+
+                _Vertices[smallIndex].z = (float)avg;
+
+                // Update UV mapping with CDRP
+                Vector2 colorSpacePoint = insta.GetColorMapPosForDepthPos(new Vector2(x,y));
+                Color32[] a = insta.getColor();
+                _Colors[smallIndex] = a[(int)(colorSpacePoint.y * KinectWrapper.Constants.ColorImageWidth + colorSpacePoint.x)];
+                _UV[smallIndex] = new Vector2(colorSpacePoint.x / colorWidth, colorSpacePoint.y / colorHeight);
+            }
+        }
+
+        _Mesh.vertices = _Vertices;
+       _Mesh.uv = _UV;
+        if (ShowTrueColors)
+        {
+            _Mesh.colors = _Colors;
+        }
+        //_Mesh.triangles = _Triangles;
+        //_Mesh.RecalculateNormals();
+    }
+
+    private double GetAvg(ushort[] depthData, int x, int y, int width, int height)
+    {
+        double sum = 0.0;
+
+        for (int y1 = y; y1 < y + 4; y1++)
+        {
+            for (int x1 = x; x1 < x + 4; x1++)
+            {
+                int fullIndex = (y1 * width) + x1;
+
+                if (depthData[fullIndex] == 0)
+                    sum += 4500;
+                else
+                    sum += depthData[fullIndex];
+
+            }
+        }
+
+        return sum / 16;
+    }
+
+    void OnApplicationQuit()
+    {
+    }
+}
